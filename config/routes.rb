5100a1e0fdc1b9459303f4be291e12358a107328
hamza0867 <hamza0867@gmail.com>
2019-06-04 15:46:46 +0000

    Fix Bar api_development tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :bars
   scope :api, defaults: {format: :json} do
     resources :foos
+    resources :bars
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- spec/requests/api_developments_spec.rb
+++ spec/requests/api_developments_spec.rb
@@ -5,10 +5,6 @@ RSpec.describe "ApiDevelopments", type: :request do
     JSON.parse(response.body)
   end
 
-  def fixed_url(path, id)
-    path + "/" + id.to_s
-  end
-
   describe "RDBMS-backed" do
     before(:each) { Foo.delete_all }
     after(:each) { Foo.delete_all }
@@ -31,15 +27,17 @@ RSpec.describe "ApiDevelopments", type: :request do
     after(:each) { Bar.delete_all }
 
     it 'create MongoDB-backed model' do
-      #object = Bar.create(name: "test")
-      #expect(Bar.find(object.id).name).to eq("test") 
+      object = Bar.create(name: "test")
+      expect(Bar.find(object.id).name).to eq("test") 
     end
     it 'expose MongoDB-backed API resource' do
-      #object = Bar.create(name: "test")
-      #expect(bar_path).to eq("/api/bars")
-      #get bar_path(object.id)
-      #expect(response).to have_http_status(:ok) 
-      #expect(parsed_body["name"]).to eq("test") 
+      object = Bar.create(name: "test")
+      expect(bars_path).to eq("/api/bars")
+      get bar_path(object.id)
+      expect(response).to have_http_status(:ok) 
+      expect(parsed_body["name"]).to eq("test") 
+      expect(parsed_body).to include("created_at") 
+      expect(parsed_body).to include("id" => object.id.to_s) 
     end
   end
 end
