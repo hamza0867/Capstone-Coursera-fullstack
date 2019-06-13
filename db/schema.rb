3ff7175736b1f9cc20902fee98a079c301d61e3c
hamza0867 <hamza0867@gmail.com>
2019-06-13 01:25:50 +0100

    Add e2e cities, states

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_04_044126) do
+ActiveRecord::Schema.define(version: 2019_06_13_000108) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cities", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "foos", force: :cascade do |t|
     t.string "name"
