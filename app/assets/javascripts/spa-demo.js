02c35758fada6b46a6fe819b0c1b4eb8ef4e3ecd
hamza0867 <hamza0867@gmail.com>
2019-06-14 17:58:04 +0100

    Add angular to the assets pipeline

--- app/assets/javascripts/spa-demo.js
+++ app/assets/javascripts/spa-demo.js
@@ -5,3 +5,10 @@
 //= require angular-ui-router
 //= require angular-resource
 //= require angular-material
+//= require angular-animate
+//= require angular-aria
+//= require angular-messages
+//
+//= require spa-demo/app.module
+//= require spa-demo/app.router
+//= require spa-demo/app.constant
---
+++ app/assets/javascripts/spa-demo/app.module.js
@@ -0,0 +1,4 @@
+(function() {
+  'use strict';
+  angular.module('spa-demo', ['ui.router', 'ngMaterial', 'ngMessages']);
+})();
